3a65b482f17b56c9df4326335bbcff87e9a73f61
Owner <owner@Owners-MacBook-Pro.local>
2019-03-21 19:45:01 -0400

    working on products module

---
+++ W2019_MAD_3115_Group-11_IOS_Project/Controller/ProductViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProductViewController.swift
+//  W2019_MAD_3115_Group-11_IOS_Project
+//
+//  Created by Owner on 2019-03-21.
+//  Copyright © 2019 Owner. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var tblproductdetails: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ W2019_MAD_3115_Group-11_IOS_Project/Modal/Products.swift
@@ -0,0 +1,48 @@
+//
+//  Products.swift
+//  W2019_MAD_3115_Group-11_IOS_Project
+//
+//  Created by Owner on 2019-03-21.
+//  Copyright © 2019 Owner. All rights reserved.
+//
+
+import Foundation
+class Products:IDisplay
+{
+    
+    let productid:String
+    var productname:String
+    var productprice:Float
+    var productdetails=Dictionary<String,Products>()
+    init() {
+        self.productid=String()
+        self.productname=String()
+        self.productprice=Float()
+        
+    }
+    init(productid:String, productname:String, productprice:Float) {
+        self.productid = productid
+        self.productname = productname
+        self.productprice = productprice
+    }
+    func addproducts(pid:String,prod:Products)
+    {
+        productdetails.updateValue(prod, forKey: pid)
+    }
+    
+    func displaydata(msg: Bool, no: Int) {
+        print(" ")
+    }
+    
+    func displaydata() {
+        print("------------------------------------------------------------------------------")
+        print("Product ID:     ||   Product name:       ||   Product price:  ")
+        print("------------------------------------------------------------------------------")
+        
+        for (_,v) in productdetails
+        {
+            print("\(v.productid)            ||   \(v.productname)         ||           \(v.productprice)")
+            
+        }
+}
+}
