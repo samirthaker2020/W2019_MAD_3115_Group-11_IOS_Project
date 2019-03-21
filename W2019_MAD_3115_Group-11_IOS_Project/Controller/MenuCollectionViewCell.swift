82eb8d48d2612abaaee4deb9a007bdb6cf58d9cd
Owner <owner@owners-macbook-pro.local>
2019-03-21 16:38:11 -0400

    created menu screen

---
+++ W2019_MAD_3115_Group-11_IOS_Project/Controller/MenuCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCollectionViewCell.swift
+//  W2019_MAD_3115_Group-11_IOS_Project
+//
+//  Created by Owner on 2019-03-21.
+//  Copyright © 2019 Owner. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var menuimg1: UIImageView!
+    
+    @IBOutlet weak var iconlabel: UILabel!
+}
